2846acd0a732460c1ab6cefd8aa381d2410855cb
dave-read <davre@microsoft.com>
2016-08-26 12:17:51 -0700

    enable VS with Node extension

---
+++ web-node/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/morgan/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/request/index.d.ts" />
